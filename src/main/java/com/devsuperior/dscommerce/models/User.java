0401a2e3bd409feff4cefd07f18235b8e0e4819f
Daniel Alisson <130872709+Daniel-Alisson@users.noreply.github.com>
2025-01-21 15:04:12 -0300

    v0.0.1
    Aprendendo sobre banco H2

---
+++ src/main/java/com/devsuperior/dscommerce/models/User.java
@@ -0,0 +1,104 @@
+package com.devsuperior.dscommerce.models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+// Configurando essa classe para uma tabela de dados relacional H2
+@Entity
+// Criando o nome da tabela
+@Table(name = "tb_user")
+public class User {
+
+    // Configurar como Id
+    @Id
+    // Configurar como Auto Imcrement
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private LocalDate birthDate;
+    private String password;
+
+    @OneToMany(mappedBy = "client")
+    List<Order> orders = new ArrayList<>();
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, String phone, LocalDate birthDate, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.birthDate = birthDate;
+        this.password = password;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "birthDate=" + birthDate +
+                ", id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
