2fc42f72df2c2dc7a9208e0b077eb8ac6445f4ab
Daniel Alisson <130872709+Daniel-Alisson@users.noreply.github.com>
2025-01-22 15:25:00 -0300

    v0.0.2
    Adicionando a classe Payment

--- src/main/java/com/devsuperior/dscommerce/models/Order.java
+++ src/main/java/com/devsuperior/dscommerce/models/Order.java
@@ -19,4 +19,58 @@ public class Order {
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
+
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
+    public Order(){
+    }
+
+    public Order(OrderStatus status, User client, Long id, Instant moment, Payment payment) {
+        this.status = status;
+        this.client = client;
+        this.id = id;
+        this.moment = moment;
+        this.payment = payment;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
 }
